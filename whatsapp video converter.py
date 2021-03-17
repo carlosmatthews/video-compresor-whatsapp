f506cbd8d33bef96caea4b4b440de01a12739f73
Carlos Matthews <cromaticofilms@gmail.com>
2021-03-17 13:39:06 -0300

    inicio

---
+++ whatsapp video converter.py
@@ -0,0 +1,6 @@
+""" programa para partir en segmentos y comprimir videos mas largos y de mayor peso que los 64 Mg que permite Whatsapp, 
+para mostrar trabajos y su visualisacion rapida.
+"""
+#TODO: DETERMINAR EL RATE DE COMPRESION A USAR EN FORMATO h264  Y SU DURACION PARA PARTIR EN SEGMENTOS MENORES A 64MB
+# probar peso en 720x480p@25 1000 Kb/s
+#TODO: BUSCAR MODULO DE PYTHON PARA CORTAR Y COMPRIMIR VIDEO. libreria MoviePY
