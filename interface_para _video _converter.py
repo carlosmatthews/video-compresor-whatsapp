f7402eb49522ddc802f0c359946c37d8aeea6a7d
Carlos Matthews <cromaticofilms@gmail.com>
2021-04-18 21:30:31 -0300

    clases para botenes basicos

--- interface_para _video _converter.py
+++ interface_para _video _converter.py
@@ -1,40 +1,41 @@
 from tkinter import*
 from tkinter import filedialog
-import whatsapp_video_converter
+#from whatsapp_video_converter import*
 
 class Aplicacion():
     def __init__(self):
-        raiz = Tk()
-        raiz.title("video compresor whatsapp")
-        raiz.geometry("300x300")
-        boton1 = Button
-        boton2 = Button
-class Button():
-    def __init__(self):
-        Button(text ="vacio",bg="pale green", command = None, font = ("Arial Bold", 15)).place(x=100,y=25)
+        self.window = Tk()
+        self.window.title("video compresor whatsapp")
+        self.window.geometry("300x200")
+        self.boton1 = Button(text ="select video",bg="pale green", command = open_file, font = ("Arial Bold", 12)).place(x=100,y=40)
+        self.boton2 = Button(text ="    convert    ", bg = "pale green", command = None, font = ("Arial Bold", 12)).place(x=100,y=80)
+        self.buton3 = Button(text ="      stop       ", bg = "pale green", command = None, font = ("Arial Bold", 12)).place(x=100,y=120)
+        self.window.mainloop()
 
 
-
-
-
-my_video = None
+mi_video = None
 def open_file():
-    my_video = filedialog.askopenfilename(initialdir = "/",title = "selecione video")
+    mi_video = filedialog.askopenfilename(initialdir = "/",title = "selecione video")
+    
+def inicio():
+    app = Aplicacion()    
+    return 0
+
+if __name__ == '__main__':
+    inicio()
+
+
+
+
     
 
-    
-app = Aplicacion()    
-
-    
-
-
+"""
 
 Button(text ="selecione video",bg="pale green", command = open_file, font = ("Arial Bold", 15)).place(x=100,y=25)
 
 Button(text = "start", bg = "green", command = start, font = ("Arial Bold", 15)).place(x=100,y=80)
+"""
 
-
-window.mainloop()
 
 
 
