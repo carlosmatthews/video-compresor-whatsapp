d04f99f5286dc409b8ce33e28e60685409128bdb
Carlos Matthews <cromaticofilms@gmail.com>
2021-05-09 20:05:59 -0300

    gui funcionado con boton stop

--- video_converter_GUI.py
+++ video_converter_GUI.py
@@ -2,48 +2,45 @@ from tkinter import*
 from tkinter import filedialog
 from backend import*
 import threading
-import multiprocessing
+import multiprocessing, threading
 
 class VideoConverterGUI():
  
     def __init__(self):
+        self.mi_video = None
         self.window = Tk()
         self.window.title("video compresor whatsapp")
         self.window.geometry("300x200")
         self.boton1 = Button(text ="select video",bg="pale green", command = self.open_file, font = ("Arial Bold", 12))
         self.boton1.pack(padx=10, pady=10)
-        self.boton2 = Button(text ="    convert    ", bg = "pale green", command = self.crear_proceso, font = ("Arial Bold", 12))
+        self.boton2 = Button(text ="    convert    ", bg = "pale green", command = self.crear_hilo, font = ("Arial Bold", 12))
         self.boton2.pack(padx=10, pady=10)
-        self.boton3 = Button(text ="      stop       ", bg = "pale green", command = self.terminar_proceso, font = ("Arial Bold", 12))
+        self.boton3 = Button(text ="      stop       ", bg = "pale green", command = self.terminar_hilo, font = ("Arial Bold", 12))
         self.boton3.pack(padx=10, pady=10)
         self.window.mainloop()
                       
     def open_file(self):
         self.mi_video = filedialog.askopenfilename(initialdir = "/",title = "selecione video")
-
     
-   
-      
-    def crear_proceso(self):
-        def llamando_a_start(self):
-            start(self.mi_video)    
-        
-        self.proceso1 = multiprocessing.Process(target = llamando_a_start) #crea un hilo(objeto) para que la convercion se ejecute en paralelo
-        self.proceso1.start() #inicio el hilo y llamada a funcion start
-        
     
-    def terminar_proceso(self):
-       self.proceso1.terminate()
-    
+    def llamando_a_start(self):
+        start(self.mi_video)    
+    def crear_hilo(self):
+        self.hilo1 = threading.Thread(target=self.llamando_a_start)
+        self.hilo1.start()
+    def terminar_hilo(self):
+        self.hilo1._stop() # este es una metodo no documentado que podria depreciarce  
  
-    
+ 
+
 def inicio():
     app = VideoConverterGUI()    
-    return 0
+    return app
+
 
 
 if __name__ == '__main__':
-    inicio()
+    app = inicio()
 
 
 
